ea84d2a5de6d5c682404cb5276a32855b414340d
Patrick Howe <skills697@gmail.com>
2024-03-29 22:52:40 -0500

    Begin Surf Map Workflow

--- VMFGeneration/Program.cs
+++ VMFGeneration/Program.cs
@@ -7,18 +7,44 @@ namespace VMFGenerator
     {
         static void Main(string[] args)
         {
-            string directory = @"C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\sdk_content\maps";
+            #if DEBUG
+                args = new string[] { @"C:\PS\visual_studio\surf-map-gen\Output"};
+            #endif
+
+            string directory = "";
             if(args.Length > 0)
             {
                 if(Directory.Exists(args[0]))
                 {
                     directory = args[0];
                 }
+                else
+                {
+                    Console.WriteLine("No Way Jose");
+                    return;
+                }
             }
+            else
+            {
+                Console.WriteLine("Missing Arguments: [1] Output File Destination Directory, [2] Input File Path");
+                return;
+            }
+
+            if(string.IsNullOrEmpty(directory))
+            {
+                Console.WriteLine("Invalid Arguments");
+                return;
+            }
+
             string vmfPath = directory + @"\GeneratedMap.vmf";
 
-            Generator gen = new Generator();
-            File.WriteAllText(vmfPath, gen.Generate(vmfPath));
+            if(File.Exists(vmfPath))
+            {
+                // move and increment name by 1
+                File.Move(vmfPath, directory + @"\GeneratedMap" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".vmf");
+            }
+
+            File.WriteAllText(vmfPath, SurfController.GenerateSurfMap());
 
             //Thanks to Pikapi for reminding me to add this
             Console.WriteLine("Press any key to continue");
---
+++ VMFGeneration/SurfController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace VMFGenerator
+{
+    public static class SurfController
+    {
+        public static string GenerateSurfMap()
+        {
+            string res = "";
+
+            return res;
+        }
+    }
+}
